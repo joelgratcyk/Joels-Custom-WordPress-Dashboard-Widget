36f3deac556f5fb1247f81db26015fa4bb0459ad
Joel Gratcyk <joel@gratcyk.com>
2023-10-21 08:21:08 -0500

    Update copypasteto_functions.php

--- copypasteto_functions.php
+++ copypasteto_functions.php
@@ -1,14 +1,19 @@
-**
- * Custom WordPress Dashboard Widget
+<?php
+/**
+ * Plugin Name: Custom Dashboard Widget
+ * Description: Adds a custom widget to the WordPress dashboard.
+ * Version: 1.31
+ * Author: Fried Egg Burger
+ * Author URI: https://friedeggburger.com
  */
-add_action('wp_dashboard_setup', 'custom_dashboard_widget');
-  
+
+// Custom WordPress Dashboard Widget
 function custom_dashboard_widget() {
-global $wp_meta_boxes;
- 
+    global $wp_meta_boxes;
+
 wp_add_dashboard_widget('custom_help_widget', 'WIDGET TITLE HERE', 'custom_dashboard_help');
 }
- 
+
 function custom_dashboard_help() {
 echo '<p>INSERT CONTENT HERE</p>';
 }
